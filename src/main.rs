4095a3f0ebc5ecef6ae892a8a87c6da439a68c71
raidshift <laurenz.zielinski@gmail.com>
2024-08-01 12:15:04 +0200

    build_2

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ const COMMANDS: [&str; 4] = ["ea", "e", "da", "d"];
 
 const STD_ERR_INFO: &str = "
 
-NoXS V1.2.R (https://github.com/raidshift/noxs)
-
-Usage:
-   noxs <cmd> <in_file> <out_file>
-                     or
-   noxs <cmd> <in_file> <out_file> <passw_file>
-
-Commands:
-   e = encrypt  |  ea = encrypt & base64-encode
-   d = decrypt  |  da = base64-decode & decrypt
+      ███████        NoXS V1.2.R (https://github.com/raidshift/noxs)
+    ███     ███
+   ███       ███     Usage:
+   ███       ███        noxs <cmd> <in_file> <out_file>
+███████████████████                        or
+███████████████████     noxs <cmd> <in_file> <out_file> <passw_file>
+████████   ████████
+████████   ████████  Commands:
+███████████████████     e = encrypt  |  ea = encrypt & base64-encode
+ █████████████████      d = decrypt  |  da = base64-decode & decrypt
 
 ";
 
