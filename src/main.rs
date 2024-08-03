eebb53e2e210585ea4d3379b9d32bd5e4345e80a
raidshift <laurenz.zielinski@gmail.com>
2024-08-03 19:42:46 +0200

    build_2

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use std::{env, fs, io};
 const COMMANDS: [&str; 4] = ["ea", "e", "da", "d"];
 
 const STD_ERR_INFO: &str = "
-      ███████        NoXS V1.2.R (https://github.com/raidshift/noxs)
-    ███     ███
-   ███       ███     Usage:
-   ███       ███        noxs <cmd> <in_file> <out_file>
-███████████████████                        or
-███████████████████     noxs <cmd> <in_file> <out_file> <passw_file>
-████████   ████████
-████████   ████████  Commands:
-███████████████████     e = encrypt  |  ea = encrypt & base64-encode
- █████████████████      d = decrypt  |  da = base64-decode & decrypt
+    od$$$$oo      NoXS V1.2.R (https://github.com/raidshift/noxs)
+   $$*°  °?$$
+  d$$      ?$b    Usage:
+  d$b      d$b     noxs <cmd> <in_file> <out_file>
+d$$$$$$$$$$$$$$o                      or
+$$$$$$$$$$$$$$$$   noxs <cmd> <in_file> <out_file> <passw_file>
+$$$$$$$  $$$$$$$
+$$$$$$$  $$$$$$$  Commands:
+$$$$$$$$$$$$$$$$    e = encrypt  |  ea = encrypt & base64-encode
+?$$$$$$$$$$$$$$$    d = decrypt  |  da = base64-decode & decrypt
 ";
 
 const STD_ERR_FILE_NOT_FOUND: &str = "File not found";
