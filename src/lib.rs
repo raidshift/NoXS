9c87715402b06031a2ea361fe9ad0bd46af8cbcd
raidshift <laurenz.zielinski@gmail.com>
2024-08-09 09:37:20 +0200

    build_2

--- src/lib.rs
+++ src/lib.rs
@@ -56,7 +56,7 @@ pub fn derive_key_with_salt(password: &[u8]) -> ([u8; ARGON2ID_KEY_LEN], [u8; AR
     (key, salt)
 }
 
-pub fn encrypt(
+fn encrypt(
     key: &[u8; ARGON2ID_KEY_LEN],
     salt: &[u8; ARGON2ID_SALT_LEN],
     plaintext: &[u8],
@@ -77,7 +77,7 @@ pub fn encrypt_with_password(
     Ok((*salt, encrypt(key, salt, plaintext)?))
 }
 
-pub fn decrypt(
+fn decrypt(
     key: &[u8; ARGON2ID_KEY_LEN],
     nonce: &[u8; CHACHAPOLY_NONCE_LEN],
     ciphertext: &[u8],
@@ -99,4 +99,152 @@ pub fn decrypt_with_password(
             .unwrap(),
         ciphertext,
     )
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PASSWORD_HEX: &str = "b102a3049c060f";
+    const KEY_HEX: &str = "ba49c1d86ab3b281e3cafe626e84274d6600504ec8bb072149b356ce1faea48b";
+    const PLAINTEXT_HEX: &str = "6de01091d749f189c4e25aa315b314aa";
+    const VERSION_HEX: &str = "01";
+    const SALT_HEX: &str = "01020304a71ea4bf40414e434bc54649";
+    const TAG_HEX: &str = "adcacd100c31dc5b2fa4c1f4575e684f";
+    const CIPHERTEXT_HEX: &str = "91352cd42cf496937b700a902c01d9d4adcacd100c31dc5b2fa4c1f4575e684f";
+    const CIPHERTEXT_COMBINED_HEX: &str = "0101020304a71ea4bf40414e434bc5464991352cd42cf496937b700a902c01d9d4adcacd100c31dc5b2fa4c1f4575e684f";
+
+    #[test]
+    fn kdf1() {
+        let password = hex::decode(PASSWORD_HEX).unwrap();
+        let salt = hex::decode(SALT_HEX).unwrap();
+        let key = derive_key(&password, &salt.try_into().unwrap());
+        assert_eq!(hex::encode(key), KEY_HEX);
+    }
+
+    #[test]
+    fn kdf2() {
+        let password = vec![];
+        let salt = vec![0; 16];
+        let key = derive_key(&password, &salt.try_into().unwrap());
+        assert_eq!(key.len(), 32);
+    }
+
+    #[test]
+    fn encrypt1() {
+        let key = hex::decode(KEY_HEX).unwrap();
+        let salt = hex::decode(SALT_HEX).unwrap();
+        let plaintext = hex::decode(PLAINTEXT_HEX).unwrap();
+        let ciphertext: Vec<u8> = encrypt(
+            &key.try_into().unwrap(),
+            &salt.try_into().unwrap(),
+            &plaintext,
+        )
+        .unwrap();
+        assert_eq!(hex::encode(ciphertext), CIPHERTEXT_HEX);
+    }
+
+    #[test]
+    fn encrypt3() {
+        let password = vec![];
+        let plaintext = vec![];
+        let (_, ciphertext) = encrypt_with_password(&password, &plaintext).unwrap();
+        assert_eq!(ciphertext.len(), CHACHAPOLY_TAG_LEN);
+    }
+
+    #[test]
+    fn decrypt1() {
+        let key = hex::decode(KEY_HEX).unwrap();
+        let data = hex::decode(CIPHERTEXT_COMBINED_HEX).unwrap();
+        data.get(..VERSION_BYTES.len())
+            .filter(|&v| v == &VERSION_BYTES[..])
+            .unwrap();
+        let salt = data
+            .get(VERSION_BYTES.len()..VERSION_BYTES.len() + ARGON2ID_SALT_LEN)
+            .unwrap();
+        let ciphertext = data
+            .get(VERSION_BYTES.len() + ARGON2ID_SALT_LEN..)
+            .filter(|slice| slice.len() >= CHACHAPOLY_TAG_LEN)
+            .unwrap();
+        assert_eq!(hex::encode(ciphertext), CIPHERTEXT_HEX);
+        let plaintext = decrypt(
+            &key.try_into().unwrap(),
+            salt[ARGON2ID_SALT_LEN - CHACHAPOLY_NONCE_LEN..]
+                .try_into()
+                .unwrap(),
+            ciphertext,
+        )
+        .unwrap();
+        assert_eq!(hex::encode(plaintext), PLAINTEXT_HEX);
+    }
+
+    #[test]
+    fn decrypt2() {
+        let password = hex::decode(PASSWORD_HEX).unwrap();
+        let salt = hex::decode(SALT_HEX).unwrap();
+        let ciphertext = hex::decode(CIPHERTEXT_HEX).unwrap();
+        let plaintext =
+            decrypt_with_password(&password, &salt.try_into().unwrap(), &ciphertext).unwrap();
+        assert_eq!(hex::encode(plaintext), PLAINTEXT_HEX);
+    }
+
+    #[test]
+    fn decrypt3() {
+        let password = hex::decode(PASSWORD_HEX).unwrap();
+        let data = hex::decode(format!("{}{}{}", VERSION_HEX, SALT_HEX, TAG_HEX)).unwrap();
+        data.get(..VERSION_BYTES.len())
+            .filter(|&v| v == &VERSION_BYTES[..])
+            .unwrap();
+        let salt = data
+            .get(VERSION_BYTES.len()..VERSION_BYTES.len() + ARGON2ID_SALT_LEN)
+            .unwrap();
+        let ciphertext = data
+            .get(VERSION_BYTES.len() + ARGON2ID_SALT_LEN..)
+            .filter(|slice| slice.len() >= CHACHAPOLY_TAG_LEN)
+            .unwrap();
+        let result = decrypt_with_password(&password, &salt.try_into().unwrap(), &ciphertext);
+        assert!(matches!(result, Err(CipherError::DecryptionFailed)));
+    }
+
+    #[test]
+    fn decrypt4() {
+        let data = hex::decode(format!("ff{}{}", SALT_HEX, TAG_HEX)).unwrap();
+        let version = data
+            .get(..VERSION_BYTES.len())
+            .filter(|&v| v == &VERSION_BYTES[..]);
+        assert!(matches!(version, None));
+        let salt = data
+            .get(VERSION_BYTES.len()..VERSION_BYTES.len() + ARGON2ID_SALT_LEN)
+            .unwrap();
+        assert_eq!(salt.len(), ARGON2ID_SALT_LEN);
+        let ciphertext = data
+            .get(VERSION_BYTES.len() + ARGON2ID_SALT_LEN..)
+            .filter(|slice| slice.len() >= CHACHAPOLY_TAG_LEN)
+            .unwrap();
+        assert_eq!(ciphertext.len(), CHACHAPOLY_TAG_LEN);
+    }
+
+    #[test]
+    fn decrypt5() {
+        let data = hex::decode(format!("{}{}", SALT_HEX, TAG_HEX)).unwrap();
+        let ciphertext = data
+            .get(VERSION_BYTES.len() + ARGON2ID_SALT_LEN..)
+            .filter(|slice| slice.len() >= CHACHAPOLY_TAG_LEN);
+        assert!(matches!(ciphertext, None));
+    }
+
+    #[test]
+    fn combined1() {
+        let password = hex::decode(PASSWORD_HEX).unwrap();
+        let plaintext = hex::decode(PLAINTEXT_HEX).unwrap();
+        let (salt, ciphertext) = encrypt_with_password(&password, &plaintext).unwrap();
+        assert_eq!(ciphertext.len(), plaintext.len() + CHACHAPOLY_TAG_LEN);
+        let mut ciphertext_combined = Vec::new();
+        ciphertext_combined.extend_from_slice(&VERSION_BYTES);
+        ciphertext_combined.extend_from_slice(&salt);
+        ciphertext_combined.extend_from_slice(&ciphertext);
+        let plaintext2 = decrypt_with_password(&password, &salt, &ciphertext).unwrap();
+        assert_eq!(plaintext, plaintext2);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use base64::{engine::general_purpose, Engine};
+use base64::prelude::*;
 use noxs::{
     decrypt_with_password, encrypt_with_password, ARGON2ID_SALT_LEN, CHACHAPOLY_TAG_LEN,
     VERSION_BYTES,
@@ -91,7 +91,7 @@ fn main() {
         password_from_file = true;
     }
 
-    let mut data = read_data_from_file(in_path);
+    let mut in_data = read_data_from_file(in_path);
     let is_base64data;
 
     is_base64data = matches!(args[1].as_str(), "ea" | "da");
@@ -107,7 +107,7 @@ fn main() {
                 }
             }
 
-            encrypt_with_password(&password, &data)
+            encrypt_with_password(&password, &in_data)
                 .map(|(salt, ciphertext)| {
                     if is_base64data {
                         let mut combined = Vec::new();
@@ -115,10 +115,7 @@ fn main() {
                         combined.extend_from_slice(&salt);
                         combined.extend_from_slice(&ciphertext);
 
-                        write_data_to_file(
-                            out_path,
-                            general_purpose::STANDARD.encode(combined).as_bytes(),
-                        )
+                        write_data_to_file(out_path, BASE64_STANDARD.encode(combined).as_bytes())
                     } else {
                         write_io_slices_to_file(
                             out_path,
@@ -138,24 +135,25 @@ fn main() {
                 password = query_password(STD_OUT_ENTER_PASSWORD);
             }
             if is_base64data {
-                data = general_purpose::STANDARD
-                    .decode(data)
+                in_data = BASE64_STANDARD
+                    .decode(in_data)
                     .unwrap_or_else(|_| exit_with_error(STD_ERR_NOT_BASE64));
             }
 
-            data.get(..VERSION_BYTES.len())
+            in_data
+                .get(..VERSION_BYTES.len())
                 .filter(|&v| v == &VERSION_BYTES[..])
                 .unwrap_or_else(|| exit_with_error(STD_ERR_INVALID_CIPHER));
-            let salt = data
+            let salt = in_data
                 .get(VERSION_BYTES.len()..VERSION_BYTES.len() + ARGON2ID_SALT_LEN)
                 .unwrap_or_else(|| exit_with_error(STD_ERR_INVALID_CIPHER));
-            let ciphertext = data
+            let ciphertext = in_data
                 .get(VERSION_BYTES.len() + ARGON2ID_SALT_LEN..)
                 .filter(|slice| slice.len() >= CHACHAPOLY_TAG_LEN)
                 .unwrap_or_else(|| exit_with_error(STD_ERR_INVALID_CIPHER));
 
             decrypt_with_password(&password, salt.try_into().unwrap(), ciphertext)
-                .map(|decrypted_data| write_data_to_file(&out_path, &decrypted_data))
+                .map(|plaintext| write_data_to_file(&out_path, &plaintext))
                 .unwrap_or_else(|e| exit_with_error(&e.to_string()));
         }
         _ => {}
